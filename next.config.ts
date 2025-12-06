660b8453798a617ede6ae26a4bff67a05487047c
alvarobarcelona <alvaro90barcelona@gmail.com>
2025-12-06 11:26:52 +0100

    fix experimental parameter

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental:{
-    ppr:'incremental'
+    ppr:false
   }
   //The 'incremental' value allows you to adopt PPR for specific routes.
 };
